29aa4e1687994a8067f09910544d2c4135ecb37d
Dean Karn <Dean.Karn@gmail.com>
2019-05-11 11:29:15 -0700

    Updated to use go-playground/pkg (#9)
    The new go-playground/pkg provides that stack and frame information that
    can now be used without this package.

--- _examples/stack/main.go
+++ _examples/stack/main.go
@@ -3,13 +3,13 @@ package main
 import (
 	"fmt"
 
-	"github.com/go-playground/errors"
+	runtimeext "github.com/go-playground/pkg/runtime"
 )
 
 func main() {
 	// maybe you just want to grab a stack trace and process on your own like go-playground/log
 	// uses it to produce a stack trace log message
-	frame := errors.Stack()
+	frame := runtimeext.Stack()
 	fmt.Printf("Function: %s File: %s Line: %d\n", frame.Function(), frame.File(), frame.Line())
 
 	// and still have access to the underlying runtime.Frame
